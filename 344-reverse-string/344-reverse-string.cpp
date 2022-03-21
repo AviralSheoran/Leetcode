7ad77d621c9277e6ab91645388512a45a3552b53
Aviral Sheoran <71494616+AviralSheoran@users.noreply.github.com>
2022-03-21 22:04:00 +0530

    Time: 28 ms (60.86%), Space: 23.2 MB (42.10%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -4,14 +4,14 @@ public:
         // Two Approaches
         
         // Brute Force / Two pointer Approach
-//         int n = s.size();
-//         int i =0, j =n-1;
+        int n = s.size();
+        int i =0, j =n-1;
         
-//         for (i = 0; i<n/2; i++) {
-//             swap(s[i],s[j-i]);
-//         }
+        for (i = 0; i<n/2; i++) {
+            swap(s[i],s[j-i]);
+        }
         
         // Inbuilt Function in C++
-        reverse(s.begin(), s.end());
+        // reverse(s.begin(), s.end());
     }
 };
